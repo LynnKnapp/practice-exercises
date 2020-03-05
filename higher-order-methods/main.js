b94cca40ef672b3d0e56d2445f8e44acf66a7c88
Lynn Knapp <ebonyeek@gmail.com>
2020-03-04 21:52:58 -0500

    worked on higher order fucntions

--- higher-order-methods/main.js
+++ higher-order-methods/main.js
@@ -1,3 +1,4 @@
+// ***** .map ********
 
 // let array=[2,5,100]
 // let doubleNumbers = array.map(arr => {
@@ -19,32 +20,32 @@
   
 //   console.log(capitalizeNames)
 
- let celebs =[
-    {
-        name: "Angelina Jolie",
-        age: 80
-    },
-    {
-        name: "Eric Jones",
-        age: 2
-    },
-    {
-        name: "Paris Hilton",
-        age: 5
-    },
-    {
-        name: "Kayne West",
-        age: 16
-    },
-    {
-        name: "Bob Ziroll",
-        age: 100
-    }]
-    let name = document.createElement(h1)
-    name.innerText = celebs.name 
+//  let celebs =[
+//     {
+//         name: "Angelina Jolie",
+//         age: 80
+//     },
+//     {
+//         name: "Eric Jones",
+//         age: 2
+//     },
+//     {
+//         name: "Paris Hilton",
+//         age: 5
+//     },
+//     {
+//         name: "Kayne West",
+//         age: 16
+//     },
+//     {
+//         name: "Bob Ziroll",
+//         age: 100
+//     }]
+//     let name = document.createElement(h1)
+//     name.innerText = celebs.name 
 
-    let age = document.createElement(h2)
-    age.innerText = celebs.age
+//     let age = document.createElement(h2)
+//     age.innerText = celebs.age
 
     // let mappedCelebs =celebs.map(name => {
     //     if(name.age >=18){
@@ -54,8 +55,134 @@
     //     }
     // })
 
-    console.log(mappedCelebs)
-   
+    // console.log(mappedCelebs)
 
+ // *****  .filter ******
+
+//   const arr =[3,6,8,2]
+//   const greaterThanFive =arr.filter(num => num > 5)
+//   const onlyEvens = arr.filter(num => num % 2 === 0)  
+  
+  
+//   console.log(onlyEvens)
+
+//   const arr = ["dog", "wolf", "by", "family", "eaten", "camping"]
+//   const fiveCharactersOrFewerOnly = arr.filter(item =>{
+//         if(item.length <= 5){
+//             return true
+//         }
+//   })
+  
+//   console.log(fiveCharactersOrFewerOnly)
+//  const peopleArr = [
+//     { name: "Angelina Jolie", member: true },
+//     { name: "Eric Jones", member: false },
+//     { name: "Paris Hilton", member: true },
+//     { name: "Kayne West", member: false },
+//     { name: "Bob Ziroll", member: true }]
+
+//   const peopleWhoBelongToTheIlluminati = peopleArr.filter(person =>{
+//       if(person.member !== true){
+//           return person.name + " can't go to the club" 
+//       }
+//   })
+
+//   console.log(peopleWhoBelongToTheIlluminati)
+
+//  const peopleArr=[
+//     { name: "Angelina Jolie", age: 80 },
+//     { name: "Eric Jones", age: 2 },
+//     { name: "Paris Hilton", age: 5 },
+//     { name: "Kayne West", age: 16 },
+//     { name: "Bob Ziroll", age: 100 }]
+
+// const ofAge = peopleArr.filter(person =>{
+//     if(person.age >= 18){
+//         return person.name + 'can go '
+//     }
+// })
+  
+//   console.log(ofAge)
+  
+ //.some   
+ 
+// const arr =[1,3,5,2,90,20]
+
+//     arr.sort((a, b) => a-b)
+//     arr.sort((a,b) => b-a)
+  
+//   console.log(arr)
+
+// const arr =[ "dog", "wolf", "by", "family", "eaten"]
+//   arr.sort((a, b) =>{
+//       return a.length - b.length
+//   })
+//   console.log(arr)
+
+//   function lengthSort(arr) {
+//     return arr.sort(function (a, b){
+//            return a.length - b.length
+//        })
+//    }
+   
+//    console.log(lengthSort(["dog", "wolf", "by", "family", "eaten"])) 
+//    arr.sort((a, b)=>{
+//         if( a < b ){
+//             return -1
+            
+//         }else if(a > b){
+//             return +1
+//         }
+//    })
+// console.log(arr)
+// const arr = [
+// { name: "Quiet Samurai", age: 22 },
+// { name: "Arrogant Ambassador", age: 100 },
+// { name: "Misunderstood Observer", age: 2 },
+// { name: "Unlucky Swami", age: 77 }]
+
+//  const byAge = arr.sort((a, b) =>{
+//         if(a.age < b.age){
+//             return -1
+//         }else if( a.age > b.age){
+//             return +1
+//         }
 
         
+//     })
+  
+  
+//   console.log(byAge)
+
+//.reduce
+
+
+ 
+// function stringConcat(arr) {
+//     return arr.reduce((total,num) => total.toString() + num.toString())
+// }
+ 
+//  console.log(stringConcat([1,2,3]))
+var voters = [
+    {name:'Bob' , age: 30, voted: true},
+    {name:'Jake' , age: 32, voted: true},
+    {name:'Kate' , age: 25, voted: false},
+    {name:'Sam' , age: 20, voted: false},
+    {name:'Phil' , age: 21, voted: true},
+    {name:'Ed' , age:55, voted:true},
+    {name:'Tami' , age: 54, voted:true},
+    {name: 'Mary', age: 31, voted: false},
+    {name: 'Becky', age: 43, voted: false},
+    {name: 'Joey', age: 41, voted: true},
+    {name: 'Jeff', age: 30, voted: true},
+    {name: 'Zack', age: 19, voted: false}
+];
+
+const totalVotes = voters.reduce((final, voter) =>{
+        if(voter.voted){
+            final ++
+        }
+        return final
+ }, 0)
+
+ console.log(totalVotes)
